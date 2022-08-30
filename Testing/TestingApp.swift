475a33c5cd12c5df183d0d874838d11cd0806330
Alex Marchant <alex_marchant@outlook.com>
2022-08-30 16:03:28 +0100

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Alex Marchant on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
