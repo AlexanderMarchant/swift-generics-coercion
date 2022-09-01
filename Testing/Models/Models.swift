146cee808f21ed0363bf3b33a4c555e04e6f0ad9
Alex Marchant <alex_marchant@outlook.com>
2022-09-01 14:05:32 +0100

    Update to use classes instead of protocols for Folder/File

--- Testing/Models/Models.swift
+++ Testing/Models/Models.swift
@@ -10,90 +10,84 @@ import Foundation
 protocol ItemProtocol {
     var name: String { get }
 }
-protocol FolderProtocol: ItemProtocol {
-    var numberOfChildren: Int { get }
+
+class Folder: ItemProtocol {
+    
+    var name: String
+    
+    var numberOfChildren: Int
+    
+    init(name: String, numberOfChildren: Int)
+    {
+        self.name = name
+        self.numberOfChildren = numberOfChildren
+    }
+    
 }
-protocol FileProtocol : ItemProtocol {
-    var ext: String { get }
+class File: ItemProtocol {
+    
+    // MARK: ItemProtocol Conformance
+    var name: String
+    
+    // MARK: FileProtocol Conformance
+    var ext: String
+    
+    init(name: String, ext: String)
+    {
+        self.name = name
+        self.ext = ext
+    }
 }
 
 // MARK: Folder Classes
 
-class SharedFolder: FolderProtocol
+class SharedFolder: Folder
 {
-    // MARK: ItemProtocol Conformance
-    var name: String
-    
-    // MARK: FolderProtocol Conformance
-    var numberOfChildren: Int
-    
     var shareId: UUID
     var sharedTo: [String]
     
     init(name: String, numberOfChildren: Int, shareId: UUID, sharedTo: [String])
     {
-        self.name = name
-        self.numberOfChildren = numberOfChildren
         self.shareId = shareId
         self.sharedTo = sharedTo
+        super.init(name: name, numberOfChildren: numberOfChildren)
     }
 }
 
-class PrivateFolder: FolderProtocol
+class PrivateFolder: Folder
 {
-    // MARK: ItemProtocol Conformance
-    var name: String
-    
-    // MARK: FolderProtocol Conformance
-    var numberOfChildren: Int
-    
     var password: String
     var isLocked: Bool
     
     init(name: String, numberOfChildren: Int, password: String, isLocked: Bool)
     {
-        self.name = name
-        self.numberOfChildren = numberOfChildren
         self.password = password
         self.isLocked = isLocked
+        super.init(name: name, numberOfChildren: numberOfChildren)
     }
 }
 
 
 // MARK: File Classes
 
-class TextFile: FileProtocol
+class TextFile: File
 {
-    // MARK: ItemProtocol Conformance
-    var name: String
-    
-    // MARK: FileProtocol Conformance
-    var ext: String
-    
     var text: String
     
     init(name: String, ext: String, text: String)
     {
-        self.name = name
-        self.ext = ext
         self.text = text
+        super.init(name: name, ext: ext)
     }
 }
 
-class ImageFile: FileProtocol
+class ImageFile: File
 {
-    // MARK: ItemProtocol Conformance
-    var name: String
-    
-    // MARK: FileProtocol Conformance
-    var ext: String
-    
     var imageData: Data
     
     init(name: String, ext: String, imageData: Data)
     {
-        self.name = name
-        self.ext = ext
         self.imageData = imageData
+        super.init(name: name, ext: ext)
     }
 }
